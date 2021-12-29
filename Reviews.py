0794cd25700809575963ebb11498aeaf41fe3e37
Kristin Moewes <kristin.moewes@kolibrigames.com>
2021-12-29 14:32:49 +0100

    Last Code Cleanup

--- Reviews.py
+++ Reviews.py
@@ -12,29 +12,28 @@ def download_url_content(url):
  
 URL = "https://play.google.com/store/apps/details?id=com.fluffyfairygames.idleminertycoon&gl=DE&showAllReviews=true"  # URL is a constant is a vriable on a module level that should never be changed by code
                                                   
-data = download_url_content(url=URL)                                          # assigning to variable keeps code clean and readable
 
-def bs_parsing_data(data=data):
+def bs_parsing_data(data):
     soup = BeautifulSoup(data, "html.parser")  
     comment_soup=soup.find("script", string=re.compile("vaf_game"))               # Every comment seems to include a type of "vaf_game" so this should work longtime
     return str(comment_soup)
 
-def cleanup():
-    crop=bs_parsing_data().split('data:')[-1]
-    crop_complete=crop.removesuffix(', sideChannel: {}});</script>')              # can only be used on strings!!
-    comment_collection= json.loads(crop_complete)                                 # parsed to list
+def cleanup(data):
+    crop = data.split('data:')[-1]
+    crop_complete = crop.removesuffix(', sideChannel: {}});</script>')            # can only be used on strings!!
+    comment_collection = json.loads(crop_complete)                                # parsed to list
     del comment_collection[-1]                                                    # delete last entry in list cause it has nothing to do with comments themselves
     return comment_collection[0]
 
 
-def create_list():
+def create_list(data):
     comment_list= []
-    for comment in cleanup():                                                     # already accessing level 0 by default
+    for comment in data:                                                          # already accessing level 0 by default
         if isinstance(comment[7],list):                                           # check if that instance is a list or None, if None, write new list with 2 None instances, if list, return list valies
             reply_details = comment[7]
         else: 
             reply_details = [None,None] 
-        row={
+        row = {
             'pk': comment[0],                                                     # primary_key == unique identifier for every single row, cannot be duplicate
             'Name': comment[1][0],
             'Stars': comment[2],
@@ -46,9 +45,14 @@ def create_list():
         comment_list.append(row)
     return comment_list
 
-def data_frame():        
-    df=pd.DataFrame(create_list())
+def create_df_from_list(data_list):        
+    df=pd.DataFrame(data_list)
     return df
 
-print(data_frame())
+if __name__ == "__main__":
+    url_content = download_url_content(url=URL)                                   # assigning to variable keeps code clean and readable
+    parsed_data = bs_parsing_data(data = url_content)
+    clean_data = cleanup(data = parsed_data)
+    comments = create_list(data = clean_data)
+    print(create_df_from_list(data_list = comments))
 
